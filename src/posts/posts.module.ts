73fcad8850bfefd09d671ffa291b78568df2eb31
Yuzu-io <1581523609@qq.com>
2023-03-15 22:54:49 +0800

    第一次提交

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,9 +1,12 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
+import { PostsEntity } from './entity/posts.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PostsEntity])],
   controllers: [PostsController],
-  providers: [PostsService]
+  providers: [PostsService],
 })
 export class PostsModule {}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { PostsEntity } from './entity/posts.entity';
+import { HttpException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    @InjectRepository(PostsEntity)
+    private readonly postsRepository: Repository<PostsEntity>,
+  ) {}
+
+  async create(posts: Partial<PostsEntity>): Promise<PostsEntity> {
+    const { title } = posts;
+    if (!title) {
+      throw new HttpException('缺少文章标题', 401);
+    }
+    const doc = await this.postsRepository.findOne({ where: { title } });
+    if (doc) {
+      throw new HttpException('文章已存在', 401);
+    }
+    return await this.postsRepository.save(posts);
+  }
+}
